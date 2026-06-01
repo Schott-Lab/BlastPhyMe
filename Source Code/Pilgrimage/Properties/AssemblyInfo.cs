30135b7e14fa4dce7e02affc019a3a6cb3f734ec
Alexis Justin Ballesteros <aj.one1599@gmail.com>
2026-06-01 11:32:49 -0400

    Add installer project, update solution, and installation files
    Add BlastPhyMeSetup.vdproj (installer) and register it in BlastPhyMe.sln, removing references to the old x64 setup. Update solution configuration entries for the new setup project. Adjust Visual Studio workspace metadata and DocumentLayout to reference Pilgrimage (replacing ChangLab) and update Pilgrimage project files. Include updated .vs workspace/Copilot index and chat files; remove obsolete PilgrimageSetupx64 files and certificate.

--- Source Code/Pilgrimage/Properties/AssemblyInfo.cs
+++ Source Code/Pilgrimage/Properties/AssemblyInfo.cs
@@ -22,9 +22,9 @@ using System.Runtime.InteropServices;
 #else
 [assembly: AssemblyConfiguration("Configuration Not Specified")]
 #endif
-[assembly: AssemblyCompany("Chang Lab")]
+[assembly: AssemblyCompany("Schott Lab")]
 [assembly: AssemblyProduct("BlastPhyMe")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015, Chang Lab")]
+[assembly: AssemblyCopyright("Copyright ©, Schott Lab")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.12")]
-[assembly: AssemblyFileVersion("1.5.0.12")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
